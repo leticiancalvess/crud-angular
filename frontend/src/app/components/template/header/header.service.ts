a0c7e8aa8fd73f49a060529d21c8da93a27048eb
leticiancalves <leticiancalves1@gmail.com>
2022-09-18 14:32:08 -0300

    Updating Titles

---
+++ frontend/src/app/components/template/header/header.service.ts
@@ -0,0 +1,26 @@
+
+import { HeaderData } from '../footer/header-data.model';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+
+  private _headerData = new BehaviorSubject<HeaderData>({//BehaviorSubject emite um evento quando há uma mudança nos dados
+    title: 'Início',
+    icon: 'home',
+    routeUrl: ''
+  })
+
+  constructor() { }
+
+  get headerData(): HeaderData {
+    return this._headerData.value
+  }
+
+  set headerData(headerData: HeaderData) {
+    this._headerData.next(headerData)
+  }
+}
